2d864e07c73eeee800eb5a23a5b3a7f9a2120e72
JuanCarlos-A <juancar2805@gmail.com>
2026-09-09 01:02:34 -0500

    Updated convertirMayuscula Done

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -407,7 +407,15 @@ public class Workshop {
     public String convertirAMayusculas(String cadena) {
         // TODO: Implementar el método para convertir una cadena a mayúsculas.
         // Ejemplo: Si cadena = "hello", el resultado debería ser "HELLO".
-        return "";
+
+        char[] resultado = new char[cadena.length()];
+
+        // Recorremos cada carácter y lo convertimos a mayúscula
+        for (int i = 0; i < cadena.length(); i++) {
+            resultado[i] = Character.toUpperCase(cadena.charAt(i));
+        }
+
+        return new String(resultado);
     }
 
     // Método que convierte una cadena a minúsculas
